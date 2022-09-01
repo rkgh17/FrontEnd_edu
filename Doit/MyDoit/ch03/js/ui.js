8506b69bc603669b4f7bb3f2e198b99ad0f1ded5
황지훈 <rkgh17@gmail.com>
2022-09-01 12:50:39 +0900

    day19_4t

---
+++ Doit/MyDoit/ch03/js/ui.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$("nav li").click(function(){
+        $("#container").css("max-width", "100%");
+        let id = $(this).attr("data-rol");
+        $("nav li").removeClass("on");
+        $(this).addClass("on");
+        $(".content").removeClass("prev this next");
+        $("#"+id).addClass("this").prevAll().addClass("prev");
+        $("#"+id).nextAll().addClass("next");
+        
+    });
+});
+
